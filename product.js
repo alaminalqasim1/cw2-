98862d34643e5fb5b7735fab180d0d9deb05ec5d
alaminalqasim1 <81859625+alaminalqasim1@users.noreply.github.com>
2021-06-30 19:47:00 +0100

    Update and rename products.json to product.js

---
+++ product.js
@@ -0,0 +1,99 @@
+const lessons = [
+    {
+        "id": "1",
+        "image":"images/img1.jpg" ,
+        "subject":"English",
+        "location":"kaduna",
+        "price":280,
+        "availableInventory":5  
+    },
+    {
+        "id": "2",
+        "subject":"Mathematics",
+        "location":"kwara",
+        "price":215,
+        "availableInventory":10,
+        "image":"images/img2.jpg"   
+    },
+    {
+        "id": "3",
+        "subject":"Chemistry",
+        "location":"kano",
+        "price":230,
+        "availableInventory":5,
+        "image":"images/img3.jpg"   
+    },
+    {
+        "id": "4",
+        "subject":"Physics",
+        "location":"katsina",
+        "price":110,
+        "availableInventory":5,
+        "image":"images/img4.jpg"   
+    },
+    {
+        "id": "5",
+        "subject":"Nursing",
+        "location":"sokoto",
+        "price":150,
+        "availableInventory":5,
+        "image":"images/img5.jpg"   
+    },
+    {
+        "id": "6",
+        "subject":"Medicine",
+        "location":"jos",
+        "price":200,
+        "availableInventory":5,
+        "image":"images/img6.jpg"   
+    },
+    {
+        "id": "7",
+        "subject":"Biomedical",
+        "location":"San lagos",
+        "price":110,
+        "availableInventory":5,
+        "image":"images/img7.jpg"   
+    },
+    {
+        "id": "8",
+        "subject":"Geography",
+        "location":"jigawa",
+        "price":120,
+        "availableInventory":5,
+        "image":"images/img8.jpg"   
+    },
+    {
+        "id": "9",
+        "subject":"Agricultural ",
+        "location":"bauchi",
+        "price":180,
+        "availableInventory":5,
+        "image":"images/img9.jpg"   
+    },
+    {
+        "id": "10",
+        "subject":"Social Work",
+        "location":"ogun",
+        "price":100,
+        "availableInventory":5,
+        "image":"images/img10.jpg"   
+    },
+	{
+        "id": "11",
+        "subject":"Agricultural ",
+        "location":"zamfara",
+        "price":180,
+        "availableInventory":5,
+        "image":"images/img11.jpg"   
+    },
+    {
+        "id": "12",
+        "subject":"Social Work",
+        "location":"kebbi",
+        "price":100,
+        "availableInventory":5,
+        "image":"images/img12.jpg"   
+    },
+    
+]
